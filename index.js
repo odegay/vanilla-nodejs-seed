0e18eb6ef1bfb8ab3b361d4df26c2765b286bc78
odegay <dolegan@gmail.com>
2023-04-24 18:12:33 +0000

    Apply automated fixes

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-
 import * as http from 'http';
 import * as url from 'url';
 import * as string_decoder from 'string_decoder';
 import { handlers } from './lib/handlers';
 import { helpers } from './lib/helpers';
 import { env } from './ecosystem.config';
-import test1 from './helper1';
 
-
-function test1 (asdasd: string) {
-    return asdasd;
+function test2 (asdasd: string, sdsds: string) {
+    return asdasd + sdsds;
 }
 
 const server = http.createServer((req, res) => {
@@ -24,62 +21,3 @@ const server = http.createServer((req, res) => {
     let buffer = '';
 
     req.on('data', (data: any) => {
-        buffer += decoder.write(data);
-    });
-
-    test1('rtkj', sdsds);
-
-    req.on('end', () => {
-
-        buffer += decoder.end();
-
-        const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : handlers.notFound;
-        const data = {
-            'trimmedPath': trimmedPath,
-            'queryStringParamsObject': queryStringParamsObject,
-            'method': httpMethod,
-            'headers': headers,
-            'payload': helpers.parseJsonToObject(buffer)
-        };
-
-        chosenHandler(data, (statusCode: number, payload: object, contentType: string) => {
-
-            contentType = contentType ? contentType : 'json';
-            statusCode = statusCode && typeof statusCode === 'number' ? statusCode : 406;
-            let payloadString = '';
-
-            if (contentType === 'json') {
-
-                res.setHeader('Content-Type', 'application/json');
-                payload = payload && typeof payload === 'object' ? payload : {};
-                payloadString = JSON.stringify(payload);
-
-            } else if (contentType === 'html') {
-
-                res.setHeader('Content-Type', 'text/html');
-                payloadString = payload && typeof (payload) === 'string' ? payload : '';
-
-            }
-
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-            res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-            res.writeHead(statusCode);
-            res.end(payloadString);
-
-        });
-
-    });    
-
-});
-
-
-server.listen(env.port, () => {
-    console.log('\x1b[32m%s\x1b[0m', `Node.js Seed Server Started at Port ${env.port} in ${env.envName} mode!`);
-});
-
-const routes = {
-    '': handlers.index,
-    'ping': handlers.ping
-};
