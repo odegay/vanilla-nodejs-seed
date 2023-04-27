cd16f7185a6d60ba41c2e094476e60383a04c88d
robot <robot@noemail.com>
2023-04-27 17:21:26 +0000

    Apply automated fixes

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ function test2 (asdasd: string) {
 const server = http.createServer((req, res) => {
 
     const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    //const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    const trimmedPath = 'test';
+    //const path = parsedUrl.pathname;
+    const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
     const httpMethod = req.method;
     const queryStringParamsObject = parsedUrl.query;
     const headers = req.headers;
