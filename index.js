60971b46dbb9500c40ec58239f074c5a5f02bf7e
robot <robot@noemail.com>
2023-04-27 11:24:50 +0000

    Apply automated fixes

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-
 import * as http from 'http';
 import * as url from 'url';
 import * as string_decoder from 'string_decoder';
 import { handlers } from './lib/handlers';
 import { helpers } from './lib/helpers';
 import { env } from './ecosystem.config';
-import test1 from './helper1';
 
-
-function test1 (asdasd: string) {
-    return asdasd;
+function test2 (asdasd: string, sdsds: string) {
+    return asdasd + sdsds;
 }
 
 const server = http.createServer((req, res) => {
@@ -27,7 +24,7 @@ const server = http.createServer((req, res) => {
         buffer += decoder.write(data);
     });
 
-    test1('rtkj', sdsds);
+    test2('rtkj', sdsds);
 
     req.on('end', () => {
 
